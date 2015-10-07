df9abe1db219025b30376bc0f2bdf93efd1c475e
Ryan Yogan <ryanyogan@gmail.com>
2015-10-07 10:09:24 -0500

    create a small helper to return the domain from a url

--- client/components/posts/post_item.jsx
+++ client/components/posts/post_item.jsx
@@ -1,11 +1,17 @@
 PostItem = React.createClass({
+  returnDomain() {
+    let a = document.createElement('a');
+    a.href = this.props.post.url;
+    return a.hostname;
+  },
+
   render() {
     const post = this.props.post;
 
     return (
       <div className='post'>
         <div className='post-content'>
-          <h3><a href={post.url}>{post.title}</a><span>{post.domain}</span></h3>
+          <h3><a href={post.url}>{post.title}</a><span>{this.returnDomain()}</span></h3>
         </div>
       </div>
     );
